08aed8d0d70bbdcf790252d2cc0b382de49d2b34
linhkhanhhoang <hoangkhanhlinh010304@gmail.com>
2025-04-12 00:50:13 -0400

    updated index.js

--- client/streaming-client/src/index.js
+++ client/streaming-client/src/index.js
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import Home from "./Home"
+import Room from "./Room"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/room/:roomId" element={<App />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="room/:roomId" element={<Room />} />
+          </Route>
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
